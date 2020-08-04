1d9f99a8e34d3183408caaf9ee265c3e76af0c1f
doob187 <amderkum@gmail.com>
2020-08-04 13:12:13 +0200

    healthcheck add plex part

--- healthcheck-plex_autoscan.sh
+++ healthcheck-plex_autoscan.sh
@@ -14,25 +14,27 @@ else
     server_pass=$(grep SERVER_PASS ${PLEX_AUTOSCAN_CONFIG} | awk -F '"' '{print $4}')
     url="http://${server_ip}:${server_port}/${server_pass}"
     curl --silent --show-error -f $url > /dev/null || exit 1
-fi
-
-PLEX_AUTOSCAN_CONFIG=${PLEX_AUTOSCAN_CONFIG}
-PAGE=$(cat ${PLEX_AUTOSCAN_CONFIG} | grep '"PLEX_LOCAL_URL":' | awk -F\" '{print $4}')
-TOKEN=$(cat ${PLEX_AUTOSCAN_CONFIG} | grep '"PLEX_TOKEN":' | awk -F: '{print $2}' | awk -F\" '{print $2}')
-PGSELFTEST=$(curl -LI "${PAGE}/system?X-Plex-Token=${TOKEN}" -o /dev/null -w '%{http_code}\n' -s)
-
-######## FUNCTIONS ##########
-if [ -f ${PLEX_AUTOSCAN_CONFIG} ]
-   then
-   if [[ ${PGSELFTEST} -le 200 && ${PGSELFTEST} -gt 299 ]]
-      then
-      echo " -> PLEX down or Token missmatched "
-      echo " -> next check for accesible in 5 seconds"
-      sleep 5
-        if [[ ${PGSELFTEST} -le 200 && ${PGSELFTEST} -gt 299 ]]; then
-        echo " second check also failed "
-        echo " exit now "
-        exit 1
-        fi
-   fi
+    PAGE=$(cat ${PLEX_AUTOSCAN_CONFIG} | grep '"PLEX_LOCAL_URL":' | awk -F\" '{print $4}')
+    TOKEN=$(cat ${PLEX_AUTOSCAN_CONFIG} | grep '"PLEX_TOKEN":' | awk -F: '{print $2}' | awk -F\" '{print $2}')
+    PGSELFTEST=$(curl -LI "${PAGE}/system?X-Plex-Token=${TOKEN}" -o /dev/null -w '%{http_code}\n' -s)
+    ######## FUNCTIONS ##########
+    if [ -f ${PLEX_AUTOSCAN_CONFIG} ]
+       then
+       if [[ -f ${PLEX_AUTOSCAN_CONFIG} && ${PGSELFTEST} -le 200 && ${PGSELFTEST} -gt 299 ]]
+          then
+		  echo "[ WARNING ] -> PLEX_AUTOSCAN_CONFIG missing <- [ WARNING ]"
+          echo "[ WARNING ] -> PLEX down or Token missmatched [ WARNING ]"
+          echo "[ WARNING ] -> next check for accesible in 5 seconds [ WARNING ]"
+          sleep 5
+          if [[ ${PGSELFTEST} -le 200 && ${PGSELFTEST} -gt 299 ]]; then
+            echo "[ WARNING ] second check also failed [WARNING ]"
+            echo " exit now "
+            exit 1
+          fi
+	    else
+          echo " Config and Plex works and Token matched "
+       fi
+    else
+       echo " [ WARNING ] PLEX Down or Token missmatched [ WARNING ] "
+    fi
 fi
